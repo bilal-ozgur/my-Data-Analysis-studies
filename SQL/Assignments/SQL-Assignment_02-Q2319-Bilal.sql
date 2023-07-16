9a193f6936a7183b0354c37d58e879dbb5e71e54
bilal-ozgur <bilaltalha.ozgur@gmail.com>
2023-07-16 17:10:47 +0200

    SQL Assignment 02

---
+++ SQL/Assignments/SQL-Assignment_02-Q2319-Bilal.sql
@@ -0,0 +1,27 @@
+-- SQL Assignment 2  - Q2319 Bilal Talha Ozgur
+
+
+-- 1. Product Sales
+--You need to create a report on whether customers who purchased the product named '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD' buy the product below or not.
+
+--1. 'Polk Audio - 50 W Woofer - Black' -- (other_product)
+
+--To generate this report, you are required to use the appropriate SQL Server Built-in functions or expressions as well as basic SQL knowledge.
+
+
+
+
+
+
+
+
+
+--2. Conversion Rate
+--Below you see a table of the actions of customers visiting the website by clicking on two different types of advertisements given by an E-Commerce company. Write a query to return the conversion rate for each Advertisement type.
+
+
+--a.    Create above table (Actions) and insert values,
+
+--b.    Retrieve count of total Actions and Orders for each Advertisement Type,
+
+--c.    Calculate Orders (Conversion) rates for each Advertisement Type by dividing by total count of actions casting as float by multiplying by 1.0.
