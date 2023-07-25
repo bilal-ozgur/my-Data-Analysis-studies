b26ab8f920b4c4eed7498ce39a64ad8f9f5433f1
bilal-ozgur <bilaltalha.ozgur@gmail.com>
2023-07-25 17:49:37 +0200

    SQL HAVING-PIVOT check yourself is done

--- SQL/Self Studies/SQL-HAVING-PIVOT.sql
+++ SQL/Self Studies/SQL-HAVING-PIVOT.sql
@@ -1,6 +1,7 @@
 --CHECK YOURSELF- 17
---SORU 1--
 
+--QUESTION 1--
+--Please write a query to return only the order ids that have an average amount of more than $2000. Your result set should include order_id. Sort the order_id in ascending order.
 
 SELECT	order_id
 		--AVG(quantity * list_price ) AS avg_amount
@@ -9,22 +10,11 @@ GROUP BY order_id
 HAVING	AVG(quantity * list_price ) > 2000
 ORDER BY 1
 
---SORU 2--
 
 
-
-SELECT 
-	DATENAME(WEEKDAY, order_date) AS day_of_week,
-	COUNT(order_id) AS cnt_of_order
-FROM sale.orders
-WHERE DATEDIFF(DAY, order_date, shipped_date) > 2
-GROUP BY
-	DATENAME(WEEKDAY, order_date)
-
-
-
-SELECT *
-FROM sale.orders
+--QUESTION 2--
+--Write a query that returns the count of orders of each day between '2020-01-19' and '2020-01-25'. Report the result using Pivot Table.
+--Note: The column names should be day names (Sun, Mon, etc.).
 
 
 SELECT *
@@ -38,23 +28,5 @@ FROM
 PIVOT
 	(
 	COUNT(order_id)
-	FOR day_of_week IN ([Monday],[Tuesday],[Wednesday],[Thursday],[Friday],[Saturday],[Sunday])
+	FOR day_of_week IN ([Sunday],[Monday],[Tuesday],[Wednesday],[Thursday],[Friday],[Saturday])
 	) as pvt
-
-
-
-----ALTTAN ILHAM AL--
-SELECT *
-FROM
-	(
-	SELECT 
-		DATENAME(WEEKDAY, order_date) AS day_of_week,
-		order_id
-	FROM sale.orders
-	WHERE DATEDIFF(DAY, order_date, shipped_date) > 2
-	) AS a
-PIVOT
-(
-	COUNT(order_id)
-	FOR day_of_week IN ([Monday],[Tuesday],[Wednesday],[Thursday],[Friday],[Saturday],[Sunday])
-) AS PVT
