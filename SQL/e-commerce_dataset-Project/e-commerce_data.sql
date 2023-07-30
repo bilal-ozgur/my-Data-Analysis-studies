476e4cfeb24e48d65cab68859e2d6b747ba0d4f3
bilal-ozgur <bilaltalha.ozgur@gmail.com>
2023-07-30 12:08:48 +0200

    Progress on project e-commerce-dataset

--- SQL/e-commerce_dataset-Project/e-commerce_data.sql
+++ SQL/e-commerce_dataset-Project/e-commerce_data.sql
@@ -6,38 +6,84 @@
   ORDER BY	total_qty_order DESC;
 
 
+
+
   -- 2. Find the customer whose order took the maximum time to get shipping.
 
   SELECT    TOP 1 Cust_ID, Customer_Name, Province, Region,  Order_Date, Ship_Date, DaysTakenForShipping
   FROM		e_commerce_data
   ORDER BY	DaysTakenForShipping DESC;
 
+
+
+
   -- 3. Count the total number of unique customers in January and how many of them came back every month over the entire year in 2011
 
-  WITH		JanuaryCustomers AS (
-	  SELECT    DISTINCT Cust_ID
-	  FROM		e_commerce_data
-	  WHERE		MONTH(Order_Date) = 1
-	  ), Allmonths AS (
-	  SELECT DISTINCT DATEPART(YEAR, Order_Date) AS [year],
-					  DATEPART(MONTH, Order_Date) AS [month]
-	  FROM e_commerce_data
-	  WHERE DATEPART(YEAR, Order_Date ) = 2011
-	  ), MonthlyCustomers AS (
-	  SELECT Cust_ID,
-			 COUNT(DISTINCT DATEPART(MONTH, Order_Date)) AS months_count
-	  FROM e_commerce_data
-	  WHERE DATEPART(YEAR, Order_Date) = 2011
-	  GROUP BY Cust_ID
-	  )
-	SELECT COUNT(*)
-	FROM JanuaryCustomers AS a
-	JOIN MonthlyCustomers AS b ON a.Cust_ID = b.Cust_ID
-	WHERE b.months_count = (SELECT COUNT(*) FROM Allmonths)
+--First, let's find unique customers in January;
+
+SELECT	DISTINCT Cust_ID
+FROM	e_commerce_data
+WHERE	MONTH(Order_Date) = 1;
+
+--Second, let's find how many of these customers came back month by month in 2011;
+
+SELECT
+		DATENAME(MONTH, order_date) AS month_of_year,
+	    COUNT(DISTINCT Cust_ID) AS cnt_of_order
+FROM    e_commerce_data
+WHERE   YEAR(Order_Date) = 2011 AND
+		Cust_ID IN ( SELECT	DISTINCT Cust_ID
+					FROM	e_commerce_data
+					WHERE	MONTH(Order_Date) = 1 AND
+							YEAR(Order_Date) = 2011
+					)	
+GROUP BY
+		DATENAME(MONTH, order_date);
+
+
+
+-- 4. Write a query to return for each user the time elapsed between the first purchasing and the third purchasing, in ascending order by Customer ID.
+
+-- First, let's find customers, their orders and their previous orders
+
+SELECT  Cust_ID, Order_Date, first_ord_date,
+		DATEDIFF(DAY, first_ord_date, Order_Date) AS day_diff
+FROM	(
+		SELECT	Ord_ID, Cust_ID, Order_Date,
+				ROW_NUMBER() OVER(PARTITION BY Cust_ID ORDER BY Order_Date) AS orders_rows,
+				MIN(Order_Date) OVER(PARTITION BY Cust_ID ORDER BY Order_Date) AS first_ord_date
+		FROM	e_commerce_data
+		) AS subq
+WHERE	orders_rows = 3
+ORDER BY Cust_ID;
 
 
 
 
-  SELECT    *
-  FROM		e_commerce_data
+-- 5. Write a query that returns customers who purchased both product 11 and product 14, as well as the ratio of these products to the total number of products purchased by the customer.
 
+--First, let's find customers purchased both product 11 and product 14
+SELECT		Cust_ID, 
+			SUM(Order_Quantity) AS sum_of_quantity			
+FROM		e_commerce_data
+WHERE		Prod_ID IN ('Prod_14','Prod_11')
+GROUP BY	Cust_ID
+HAVING		COUNT(DISTINCT Prod_ID) = 2;
+
+
+--Then, let's find ratio of these products to the total number of products purchased by the customer
+WITH t1 AS (
+			SELECT		Cust_ID
+						,SUM(Order_Quantity) AS sum_of_quantity			
+			FROM		e_commerce_data
+			WHERE		Prod_ID IN ('Prod_14','Prod_11')
+			GROUP BY	Cust_ID
+			HAVING		COUNT(DISTINCT Prod_ID) = 2		
+			)
+SELECT	DISTINCT 
+		a.Cust_ID, sum_of_quantity,
+		SUM(Order_Quantity) OVER(PARTITION BY b.Cust_ID) AS overall_quantity,
+		CAST(sum_of_quantity / SUM(Order_Quantity) OVER(PARTITION BY b.Cust_ID) AS decimal(3,2))  AS ratio 
+FROM	t1 AS a,
+		e_commerce_data AS B
+WHERE	a.Cust_ID = b.Cust_ID;
