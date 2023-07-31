3fe7027dac8de4db90c99bcb12112b4c49461c2f
bilal-ozgur <bilaltalha.ozgur@gmail.com>
2023-07-31 17:32:02 +0200

    Some changes on project

--- SQL/e-commerce_dataset-Project/e-commerce_data.sql
+++ SQL/e-commerce_dataset-Project/e-commerce_data.sql
@@ -1,9 +1,4 @@
 
--- DENEME -DENEME - DENEME - DENEME
--- DENEME -DENEME - DENEME - DENEME
--- DENEME -DENEME - DENEME - DENEME
-
-
 --Before starting let's convert our ID columns into the numeric values
 
 UPDATE [dbo].e_commerce_data
