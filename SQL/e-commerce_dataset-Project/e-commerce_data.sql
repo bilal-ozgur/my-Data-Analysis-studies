1b4419c169acd395b7e015884b5c6a79033dea7f
Bilal Talha Özgür <130503711+bilal-ozgur@users.noreply.github.com>
2024-07-13 19:59:09 +0200

    Add files via upload

--- SQL/e-commerce_dataset-Project/e-commerce_data.sql
+++ SQL/e-commerce_dataset-Project/e-commerce_data.sql
@@ -1,6 +1,9 @@
 
 --Before starting let's convert our ID columns into the numeric values
 
+
+
+
 UPDATE [dbo].e_commerce_data
 SET Ord_ID = SUBSTRING(Ord_ID, CHARINDEX('_', Ord_ID) + 1 , LEN(Ord_ID)),
 	Cust_ID = SUBSTRING(Cust_ID, CHARINDEX('_', Cust_ID) + 1 , LEN(Cust_ID)),
@@ -24,7 +27,7 @@ FROM		e_commerce_data
 GROUP BY	Cust_ID
 ORDER BY	num_of_orders DESC;
 
-
+SELECT * FROM e_commerce_data
 
   -- 2. Find the customer whose order took the maximum time to get shipping.
 
@@ -109,7 +112,24 @@ FROM	t1 AS a,
 		e_commerce_data AS B
 WHERE	a.Cust_ID = b.Cust_ID;
 
+-- SOLUTION 2 --
 
+SELECT	Cust_ID,
+		"sum_prod_ID_11", 
+		"sum_prod_ID_14",
+		CAST(sum_prod_ID_11 / total_quantity AS DECIMAL(10, 2)) AS 'proc_11_ratio' ,
+		CAST(sum_prod_ID_14 / total_quantity AS DECIMAL(10, 2)) AS 'proc_14_ratio'
+FROM (
+		SELECT	 Cust_ID,
+				(SELECT SUM(Order_Quantity) FROM e_commerce_data WHERE Prod_ID = '11' AND Cust_ID = A.Cust_ID) "sum_prod_ID_11",
+				(SELECT SUM(Order_Quantity) FROM e_commerce_data WHERE Prod_ID = '14' AND Cust_ID = A.Cust_ID) "sum_prod_ID_14",
+				(SELECT SUM(Order_Quantity) FROM e_commerce_data WHERE Cust_ID = A.Cust_ID) "total_quantity"
+		FROM	e_commerce_data A
+		WHERE	Prod_ID IN ('11','14')
+		GROUP BY  Cust_ID
+		HAVING COUNT(DISTINCT Prod_ID) = 2
+		--ORDER BY 1;
+	) subq
 
 
 -- Customer Segmentation
@@ -152,6 +172,7 @@ FROM	visit_logs;
 
 
 --3. For each visit of customers, create the next/prevois month of the visit as a separate column.
+  
 SELECT	*,
 	LAG(current_month) OVER(PARTITION BY Cust_ID ORDER BY current_month) AS prev_month
 FROM(	
@@ -204,6 +225,10 @@ ORDER BY 1
 --5. Categorise customers using average time gaps. Choose the most fitted labeling model for you.
 
 --SOLUTION 1 -- 
+
+SELECT * 
+FROM time_gaps
+
 
 SELECT Cust_ID, AVG(time_gap) avg_time_gap, COUNT(*) num_of_visits,
 	CASE
